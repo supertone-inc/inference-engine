4eefb3aae2994254bec72cb90f7e4f11a3c6f2c2
krazijames <krazijames@gmail.com>
2023-05-20 15:39:26 +0900

    Setup `bindgen`

--- ort-sys/build.rs
+++ ort-sys/build.rs
@@ -1,7 +1,12 @@
-use const_format::formatcp;
-use execute_command as exec;
-
 fn main() {
+    build_cpp();
+    generate_bindings();
+}
+
+fn build_cpp() {
+    use const_format::formatcp;
+    use execute_command as exec;
+
     const CMAKE_SOURCE_DIR: &str = env!("CARGO_MANIFEST_DIR");
     const CMAKE_BUILD_DIR: &str = formatcp!("{CMAKE_SOURCE_DIR}/build");
     const CMAKE_CONFIG: &str = if cfg!(debug_assertions) {
@@ -37,4 +42,28 @@ fn main() {
     println!("cargo:rerun-if-changed=CMakeLists.txt");
     println!("cargo:rerun-if-changed=../ort-cpp");
     println!("cargo:rerun-if-changed=../core-cpp");
+}
+
+fn generate_bindings() {
+    use std::path::PathBuf;
+
+    let header_path = PathBuf::from("include").join("lib.hpp");
+    let output_path = PathBuf::from("src").join("bindings.rs");
+
+    bindgen::Builder::default()
+        .header(header_path.display().to_string())
+        .clang_args(["-x", "c++", "-std=c++17"])
+        .default_enum_style(bindgen::EnumVariation::Rust {
+            non_exhaustive: false,
+        })
+        .disable_name_namespacing()
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .respect_cxx_access_specs(true)
+        .size_t_is_usize(true)
+        .generate()
+        .unwrap()
+        .write_to_file(output_path)
+        .unwrap();
+
+    println!("cargo:rerun-if-changed=include");
 }
