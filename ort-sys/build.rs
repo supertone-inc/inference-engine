d13a95a46b05671babc784082e8e440ea48d0f9a
krazijames <krazijames@gmail.com>
2023-05-20 15:34:12 +0900

    Apply CMake config

--- ort-sys/build.rs
+++ ort-sys/build.rs
@@ -1,21 +1,35 @@
 use const_format::formatcp;
-use execute_command::status as exec;
+use execute_command as exec;
 
 fn main() {
     const CMAKE_SOURCE_DIR: &str = env!("CARGO_MANIFEST_DIR");
     const CMAKE_BUILD_DIR: &str = formatcp!("{CMAKE_SOURCE_DIR}/build");
+    const CMAKE_CONFIG: &str = if cfg!(debug_assertions) {
+        "Debug"
+    } else {
+        "Release"
+    };
 
     println!("cargo:rustc-env=CMAKE_SOURCE_DIR={CMAKE_SOURCE_DIR}");
     println!("cargo:rustc-env=CMAKE_BUILD_DIR={CMAKE_BUILD_DIR}");
+    println!("cargo:rustc-env=CMAKE_CONFIG={CMAKE_CONFIG}");
 
-    exec(format!(
+    exec::status(format!(
         "cmake \
             -S {CMAKE_SOURCE_DIR} \
             -B {CMAKE_BUILD_DIR} \
+            -D CMAKE_BUILD_TYPE={CMAKE_CONFIG} \
+            -D CMAKE_CONFIGURATION_TYPES={CMAKE_CONFIG} \
             -D INFERENCE_ENGINE_ORT_BUILD_TESTS=OFF"
     ))
     .unwrap();
-    exec(format!("cmake --build {CMAKE_BUILD_DIR} --parallel")).unwrap();
+    exec::status(format!(
+        "cmake \
+            --build {CMAKE_BUILD_DIR} \
+            --config {CMAKE_CONFIG} \
+            --parallel"
+    ))
+    .unwrap();
 
     println!("cargo:rustc-link-search={CMAKE_BUILD_DIR}");
     println!("cargo:rustc-link-lib=inference-engine-ort-sys");
--- ort-sys/tests/run_cpp_tests.rs
+++ ort-sys/tests/run_cpp_tests.rs
@@ -1,20 +1,37 @@
 use const_format::formatcp;
-use execute_command::status as exec;
+use execute_command as exec;
 
 #[test]
 fn cpp() {
     const CMAKE_SOURCE_DIR: &str = env!("CMAKE_SOURCE_DIR");
     const CMAKE_BUILD_DIR: &str = env!("CMAKE_BUILD_DIR");
+    const CMAKE_CONFIG: &str = env!("CMAKE_CONFIG");
     const ORT_CPP_BUILD_DIR: &str = formatcp!("{CMAKE_BUILD_DIR}/ort-cpp");
     const TEST_PROGRAM_NAME: &str = "inference-engine-ort-test";
 
-    exec(format!(
+    exec::status(format!(
         "cmake \
             -S {CMAKE_SOURCE_DIR} \
             -B {CMAKE_BUILD_DIR} \
+            -D CMAKE_BUILD_TYPE={CMAKE_CONFIG} \
+            -D CMAKE_CONFIGURATION_TYPES={CMAKE_CONFIG} \
             -D INFERENCE_ENGINE_ORT_BUILD_TESTS=ON"
     ))
     .unwrap();
-    exec(format!("cmake --build {CMAKE_BUILD_DIR} --parallel")).unwrap();
-    exec(format!("{ORT_CPP_BUILD_DIR}/{TEST_PROGRAM_NAME}")).unwrap();
+
+    exec::status(format!(
+        "cmake \
+            --build {CMAKE_BUILD_DIR} \
+            --config {CMAKE_CONFIG} \
+            --parallel"
+    ))
+    .unwrap();
+
+    if cfg!(windows) {
+        #[rustfmt::skip]
+            exec::status(format!("{ORT_CPP_BUILD_DIR}/{CMAKE_CONFIG}/{TEST_PROGRAM_NAME}")).unwrap();
+    } else {
+        #[rustfmt::skip]
+            exec::status(format!("{ORT_CPP_BUILD_DIR}/{TEST_PROGRAM_NAME}")).unwrap();
+    }
 }
