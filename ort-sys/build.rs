2029b22b020fa565d219cbdd40d0972455231dd4
krazijames <krazijames@gmail.com>
2023-05-22 02:25:44 +0900

    Move test execution into module test

--- ort-sys/build.rs
+++ ort-sys/build.rs
@@ -15,13 +15,18 @@ fn build_cpp() {
         "Release"
     };
 
+    println!("cargo:rustc-env=CMAKE_SOURCE_DIR={CMAKE_SOURCE_DIR}");
+    println!("cargo:rustc-env=CMAKE_BUILD_DIR={CMAKE_BUILD_DIR}");
+    println!("cargo:rustc-env=CMAKE_CONFIG={CMAKE_CONFIG}");
+
     exec::status(format!(
         "cmake \
             -S {CMAKE_SOURCE_DIR} \
             -B {CMAKE_BUILD_DIR} \
             -D CMAKE_BUILD_TYPE={CMAKE_CONFIG} \
             -D CMAKE_CONFIGURATION_TYPES={CMAKE_CONFIG} \
-            -D INFERENCE_ENGINE_ORT_RUN_TESTS=OFF"
+            -D INFERENCE_ENGINE_ORT_RUN_TESTS=OFF \
+            -D INFERENCE_ENGINE_ORT_SYS_RUN_TESTS=OFF"
     ))
     .unwrap();
     exec::status(format!(
--- ort-sys/src/lib.rs
+++ ort-sys/src/lib.rs
@@ -24,7 +24,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_create_with_invalid_model_data() {
+    fn cpp() {
+        use execute_command as exec;
+
+        const CMAKE_SOURCE_DIR: &str = env!("CMAKE_SOURCE_DIR");
+        const CMAKE_BUILD_DIR: &str = env!("CMAKE_BUILD_DIR");
+        const CMAKE_CONFIG: &str = env!("CMAKE_CONFIG");
+
+        exec::status(format!(
+            "cmake \
+                -S {CMAKE_SOURCE_DIR} \
+                -B {CMAKE_BUILD_DIR} \
+                -D CMAKE_BUILD_TYPE={CMAKE_CONFIG} \
+                -D CMAKE_CONFIGURATION_TYPES={CMAKE_CONFIG} \
+                -D INFERENCE_ENGINE_ORT_SYS_RUN_TESTS=ON"
+        ))
+        .unwrap();
+
+        exec::status(format!(
+            "cmake \
+                --build {CMAKE_BUILD_DIR} \
+                --config {CMAKE_CONFIG} \
+                --parallel"
+        ))
+        .unwrap();
+    }
+
+    #[test]
+    fn create_with_invalid_model_data() {
         unsafe {
             let result = OrtInferenceEngine::create(std::ptr::null(), 0);
             assert_eq!(result.code, ResultCode::Error);
@@ -37,7 +64,7 @@ mod tests {
     }
 
     #[test]
-    fn test_create() {
+    fn create() {
         unsafe {
             let model_data = include_bytes!("../../ort-cpp/test-models/mat_mul.onnx");
             let result = OrtInferenceEngine::create(model_data.as_ptr() as _, model_data.len());
