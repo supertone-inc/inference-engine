3df8b7545711caf28ab6553238a8d109cf190856
krazijames <krazijames@gmail.com>
2023-05-22 19:24:42 +0900

    Update trait `InferenceEngine`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,11 +1,21 @@
 pub trait InferenceEngine {
-    fn input_shapes<Size>(&self) -> &[&[Size]];
-
-    fn output_shapes<Size>(&self) -> &[&[Size]];
-
-    fn run<Input, Output, Error>(
+    fn input_count(&self) -> usize;
+    fn input_shape(&self, index: usize) -> &[usize];
+    fn set_input_shape<Error>(&self, index: usize, shape: &[usize]) -> Result<(), Error>;
+    fn set_input_data<Data, Error>(
         &mut self,
-        inputs: impl AsRef<[Input]>,
-        outputs: impl AsMut<[Output]>,
+        index: usize,
+        data: impl AsRef<Data>,
     ) -> Result<(), Error>;
+
+    fn output_count(&self) -> usize;
+    fn output_shape(&self, index: usize) -> &[usize];
+    fn set_output_shape<Error>(&self, index: usize, shape: &[usize]) -> Result<(), Error>;
+    fn set_output_data<Data, Error>(
+        &mut self,
+        index: usize,
+        data: impl AsMut<Data>,
+    ) -> Result<(), Error>;
+
+    fn run<Error>(&mut self) -> Result<(), Error>;
 }
