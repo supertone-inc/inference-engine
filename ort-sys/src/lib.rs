78c01c7b3cab6e50ab5eaf915ace8980e67352bd
krazijames <krazijames@gmail.com>
2023-05-24 02:44:22 +0900

    Extract C++ test from Rust test

--- ort-sys/src/lib.rs
+++ ort-sys/src/lib.rs
@@ -14,10 +14,9 @@ impl<E: From<ResultCode>> From<ResultCode> for Result<(), E> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use std::ffi::{c_void, CStr};
     use std::ptr::{null, null_mut};
-
-    use super::*;
 
     #[derive(Debug)]
     struct Error(String);
@@ -47,33 +46,6 @@ mod tests {
                 Result::from(destroy_inference_engine(self.0)).unwrap();
             }
         }
-    }
-
-    #[test]
-    fn cpp() {
-        use execute_command as exec;
-
-        const CMAKE_SOURCE_DIR: &str = env!("CMAKE_SOURCE_DIR");
-        const CMAKE_BUILD_DIR: &str = env!("CMAKE_BUILD_DIR");
-        const CMAKE_CONFIG: &str = env!("CMAKE_CONFIG");
-
-        exec::status(format!(
-            "cmake \
-                -S '{CMAKE_SOURCE_DIR}' \
-                -B '{CMAKE_BUILD_DIR}' \
-                -D CMAKE_BUILD_TYPE={CMAKE_CONFIG} \
-                -D CMAKE_CONFIGURATION_TYPES={CMAKE_CONFIG} \
-                -D INFERENCE_ENGINE_ORT_SYS_RUN_TESTS=ON"
-        ))
-        .unwrap();
-
-        exec::status(format!(
-            "cmake \
-                --build '{CMAKE_BUILD_DIR}' \
-                --config {CMAKE_CONFIG} \
-                --parallel"
-        ))
-        .unwrap();
     }
 
     #[test]
