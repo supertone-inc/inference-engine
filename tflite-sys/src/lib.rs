2745a06883986fe79884a4902646ffbe95970c59
krazijames <krazijames@gmail.com>
2023-06-03 11:05:21 +0900

    Fix missing C++ tests in sys packages

--- tflite-sys/src/lib.rs
+++ tflite-sys/src/lib.rs
@@ -1,1 +1,13 @@
 include!("bindings.rs");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn cpp() {
+        #[cfg(not(windows))]
+        execute_command::status("./test.sh").unwrap();
+
+        #[cfg(windows)]
+        execute_command::status("./test.bat").unwrap();
+    }
+}
