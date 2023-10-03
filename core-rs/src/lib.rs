5f184fe0efe4d82ccde46ffd7bb04f28e0f3d101
krazijames <krazijames@gmail.com>
2023-10-03 16:19:59 +0900

    Move Trait implementations into `core-sys`

--- core-rs/src/lib.rs
+++ core-rs/src/lib.rs
@@ -5,61 +5,28 @@ pub trait InferenceEngine {
     fn output_count(&self) -> usize;
 
     fn input_shape(&self, index: usize) -> &[usize];
-    fn input_shapes(&self) -> Vec<&[usize]> {
-        (0..self.input_count())
-            .map(|i| self.input_shape(i))
-            .collect()
-    }
+    fn input_shapes(&self) -> Vec<&[usize]>;
+
     fn output_shape(&self, index: usize) -> &[usize];
-    fn output_shapes(&self) -> Vec<&[usize]> {
-        (0..self.output_count())
-            .map(|i| self.output_shape(i))
-            .collect()
-    }
+    fn output_shapes(&self) -> Vec<&[usize]>;
 
     fn set_input_shape(&mut self, index: usize, shape: &[usize]) -> Result<(), Error>;
-    fn set_input_shapes(&mut self, shapes: &[&[usize]]) -> Result<(), Error> {
-        shapes
-            .iter()
-            .enumerate()
-            .try_for_each(|(i, shape)| self.set_input_shape(i, shape))
-    }
+    fn set_input_shapes(&mut self, shapes: &[&[usize]]) -> Result<(), Error>;
+
     fn set_output_shape(&mut self, index: usize, shape: &[usize]) -> Result<(), Error>;
-    fn set_output_shapes(&mut self, shapes: &[&[usize]]) -> Result<(), Error> {
-        shapes
-            .iter()
-            .enumerate()
-            .try_for_each(|(i, shape)| self.set_output_shape(i, shape))
-    }
+    fn set_output_shapes(&mut self, shapes: &[&[usize]]) -> Result<(), Error>;
 
     fn input_data(&mut self, index: usize) -> &mut [f32];
-    fn input_data_all(&mut self) -> Vec<&mut [f32]> {
-        (0..self.input_count())
-            .map(|i| unsafe {
-                let data = self.input_data(i);
-                std::slice::from_raw_parts_mut(data.as_mut_ptr(), data.len())
-            })
-            .collect()
-    }
+    fn input_data_all(&mut self) -> Vec<&mut [f32]>;
+
     fn output_data(&self, index: usize) -> &[f32];
-    fn output_data_all(&self) -> Vec<&[f32]> {
-        (0..self.output_count())
-            .map(|i| self.output_data(i))
-            .collect()
-    }
+    fn output_data_all(&self) -> Vec<&[f32]>;
 
     fn set_input_data(&mut self, index: usize, data: &[f32]) -> Result<(), Error>;
-    fn set_input_data_all(&mut self, data: &[&[f32]]) -> Result<(), Error> {
-        data.iter()
-            .enumerate()
-            .try_for_each(|(i, data)| self.set_input_data(i, data))
-    }
+    fn set_input_data_all(&mut self, data: &[&[f32]]) -> Result<(), Error>;
+
     fn set_output_data(&mut self, index: usize, data: &mut [f32]) -> Result<(), Error>;
-    fn set_output_data_all(&mut self, data: &mut [&mut [f32]]) -> Result<(), Error> {
-        data.iter_mut()
-            .enumerate()
-            .try_for_each(|(i, data)| self.set_output_data(i, data))
-    }
+    fn set_output_data_all(&mut self, data: &mut [&mut [f32]]) -> Result<(), Error>;
 
     fn run(&mut self) -> Result<(), Error>;
 }
--- core-sys/src/lib.rs
+++ core-sys/src/lib.rs
@@ -53,6 +53,12 @@ macro_rules! impl_inference_engine {
                     }
                 }
 
+                fn input_shapes(&self) -> Vec<&[usize]> {
+                    (0..self.input_count())
+                        .map(|i| self.input_shape(i))
+                        .collect()
+                }
+
                 fn output_shape(&self, index: usize) -> &[usize] {
                     unsafe {
                         let mut data = null();
@@ -62,6 +68,12 @@ macro_rules! impl_inference_engine {
                         );
                         std::slice::from_raw_parts(data, size)
                     }
+                }
+
+                fn output_shapes(&self) -> Vec<&[usize]> {
+                    (0..self.output_count())
+                        .map(|i| self.output_shape(i))
+                        .collect()
                 }
 
                 fn set_input_shape(&mut self, index: usize, shape: &[usize]) -> Result<(), Error> {
@@ -75,6 +87,13 @@ macro_rules! impl_inference_engine {
                     }
                 }
 
+                fn set_input_shapes(&mut self, shapes: &[&[usize]]) -> Result<(), Error> {
+                    shapes
+                        .iter()
+                        .enumerate()
+                        .try_for_each(|(i, shape)| self.set_input_shape(i, shape))
+                }
+
                 fn set_output_shape(&mut self, index: usize, shape: &[usize]) -> Result<(), Error> {
                     unsafe {
                         Result::from(sys::inference_engine__set_output_shape(
@@ -86,6 +105,13 @@ macro_rules! impl_inference_engine {
                     }
                 }
 
+                fn set_output_shapes(&mut self, shapes: &[&[usize]]) -> Result<(), Error> {
+                    shapes
+                        .iter()
+                        .enumerate()
+                        .try_for_each(|(i, shape)| self.set_output_shape(i, shape))
+                }
+
                 fn input_data(&mut self, index: usize) -> &mut [f32] {
                     unsafe {
                         let data = sys::inference_engine__get_input_data(self.raw, index);
@@ -94,12 +120,27 @@ macro_rules! impl_inference_engine {
                     }
                 }
 
+                fn input_data_all(&mut self) -> Vec<&mut [f32]> {
+                    (0..self.input_count())
+                        .map(|i| unsafe {
+                            let data = self.input_data(i);
+                            std::slice::from_raw_parts_mut(data.as_mut_ptr(), data.len())
+                        })
+                        .collect()
+                }
+
                 fn output_data(&self, index: usize) -> &[f32] {
                     unsafe {
                         let data = sys::inference_engine__get_output_data(self.raw, index);
                         let size = self.output_shape(index).iter().product();
                         std::slice::from_raw_parts(data, size)
                     }
+                }
+
+                fn output_data_all(&self) -> Vec<&[f32]> {
+                    (0..self.output_count())
+                        .map(|i| self.output_data(i))
+                        .collect()
                 }
 
                 fn set_input_data(&mut self, index: usize, data: &[f32]) -> Result<(), Error> {
@@ -112,6 +153,12 @@ macro_rules! impl_inference_engine {
                     }
                 }
 
+                fn set_input_data_all(&mut self, data: &[&[f32]]) -> Result<(), Error> {
+                    data.iter()
+                        .enumerate()
+                        .try_for_each(|(i, data)| self.set_input_data(i, data))
+                }
+
                 fn set_output_data(&mut self, index: usize, data: &mut [f32]) -> Result<(), Error> {
                     unsafe {
                         Result::from(sys::inference_engine__set_output_data(
@@ -120,6 +167,12 @@ macro_rules! impl_inference_engine {
                             data.as_mut_ptr(),
                         ))
                     }
+                }
+
+                fn set_output_data_all(&mut self, data: &mut [&mut [f32]]) -> Result<(), Error> {
+                    data.iter_mut()
+                        .enumerate()
+                        .try_for_each(|(i, data)| self.set_output_data(i, data))
                 }
 
                 fn run(&mut self) -> Result<(), Error> {
